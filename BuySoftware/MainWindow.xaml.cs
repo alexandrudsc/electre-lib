fdc4b4bed0ecb9983f851cf450038c5273765beb
alexandrudsc <alexandru.dascalu94@gmail.com>
2018-09-01 17:19:54 +0300

    started demo wpf app

---
+++ BuySoftware/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+using System.Windows;
+
+namespace BuySoftware
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            
+            InitializeComponent();
+            electre.initializare();
+            electre.adauga_criteriu("pret", 0.4);
+            electre.adauga_criteriu("calitate", 0.3);
+            electre.adauga_criteriu("performanta", 0.2);
+            electre.adauga_criteriu("portabilitate", 0.1);
+
+            electre.adauga_alternativa("visual studio", new double[] { 7, 9, 8, 10}, 4);
+            electre.adauga_alternativa("android studio", new double[] { 10, 7, 8, 8 }, 4);
+            electre.adauga_alternativa("eclipse", new double[] { 1, 8, 6, 8 }, 4);
+            electre.adauga_alternativa("qt creator", new double[] { 7, 6, 10, 8 }, 4);
+
+            StringBuilder in_params = new StringBuilder(1000);
+            electre.afiseaza_input(in_params);
+            MessageBox.Show(in_params.ToString());
+            in_params = new StringBuilder(1000);
+            electre.ruleaza();
+            electre.afiseaza_input(in_params);
+            MessageBox.Show(in_params.ToString());
+        }
+    }
+}
---
+++ BuySoftware/electre.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuySoftware
+{
+    class electre
+    {
+
+        [DllImport("..\\..\\..\\Debug\\electre.dll", EntryPoint = "initializare")]
+        public static extern void initializare();
+
+        [DllImport("..\\..\\..\\Debug\\electre.dll", EntryPoint = "adauga_criteriu")]
+        public static extern void adauga_criteriu(string nume, double pondere);
+
+        [DllImport("..\\..\\..\\Debug\\electre.dll", EntryPoint = "adauga_alternativa")]
+        public static extern void adauga_alternativa(string nume, double[] valori, int numar_criterii);
+
+        [DllImport("..\\..\\..\\Debug\\electre.dll", EntryPoint = "ruleaza")]
+        public static extern void ruleaza();
+
+        [DllImport("..\\..\\..\\Debug\\electre.dll", EntryPoint = "afiseaza_input")]
+        public static extern void afiseaza_input(StringBuilder in_params);
+    }
+}
